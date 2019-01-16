264f3760b83bfc4ac798338b955d0cc910ef1ed6
ChanKeun Kwon <k1212keun@skuniv.ac.kr>
2019-01-16 13:17:31 +0900

    Initial commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        //Hello World 문자열 출력
+        System.out.println("Hello World!");
+    }
+}
