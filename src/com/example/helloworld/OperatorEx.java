1d87fe76ff7ab8e35ba88ebe69246fcf4a6d7e84
ChanKeun Kwon <k1212keun@skuniv.ac.kr>
2019-01-16 14:49:51 +0900

    ver1.00

--- src/com/example/helloworld/OperatorEx.java
+++ src/com/example/helloworld/OperatorEx.java
@@ -23,5 +23,14 @@ public class OperatorEx {
         System.out.println("Final: "+b);
         //증감 연산자는 가능하면 타 연산식 속에 포함시키지말자
         //헷갈림 유발
+
+//        System.out.println(4/0);//Error
+        System.out.println(4.0/0);
+
+        //현재 데이터값이 유한한 값인지 확인
+        System.out.println(Double.isFinite(4.0/0));
+        System.out.println(Double.isFinite(4.0/2));
+        //NaN
+        System.out.println(Double.isNaN(4.0/0));
     }
 }
